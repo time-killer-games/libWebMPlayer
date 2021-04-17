79c34e30f5e169f3360eec166309563dffdd7775
time-killer-games <samuelvenable@hotmail.com>
2021-04-16 21:45:47 -0400

    Update build32.sh

--- build32.sh
+++ build32.sh
@@ -1,3 +1,12 @@
+#!/bin/sh
 cd "${0%/*}"
 
-g++ "src/*" -I./include/ -o libWebMPlayer.dll -shared -static-libgcc -static-libstdc++ -static -lpng -lvorbis -logg -lwebm -lvpx -lz -m32 -Wno-narrowing
+if [ $(uname) = "Darwin" ]; then
+  clang++ "src/*" -I./include/ -o libWebMPlayer.dylib -std=c++17 -shared -I/usr/local/include -L/usr/local/lib -static -lpng -lvorbis -logg -lwebm -lvpx -lz -m32 -Wno-narrowing -fPIC
+elif [ $(uname) = "Linux" ]; then
+  g++ "src/*" -I./include/ -o libWebMPlayer.so -std=c++17 -shared -static-libgcc -static-libstdc++ -static -lpng -lvorbis -logg -lwebm -lvpx -lz -m32 -Wno-narrowing
+elif [ $(uname) = "FreeBSD" ]; then
+  clang++ "src/*" -I./include/ -o libWebMPlayer.so -std=c++17 -shared -I/usr/local/include -L/usr/local/lib -static -lpng -lvorbis -logg -lwebm -lvpx -lz -m32 -Wno-narrowing -fPIC
+else
+  g++ "src/*" -I./include/ -o libWebMPlayer.dll -std=c++17 -shared -static-libgcc -static-libstdc++ -static -lpng -lvorbis -logg -lwebm -lvpx -lz -m32 -Wno-narrowing
+fi
