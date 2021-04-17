a93188c09747820fd8a306c61e5e378b4ec4f9aa
time-killer-games <samuelvenable@hotmail.com>
2021-04-16 23:51:53 -0400

    Add files via upload

--- build32.sh
+++ build32.sh
@@ -12,6 +12,5 @@ elif [ $(uname) = "FreeBSD" ]; then
   git clone https://github.com/webmproject/libwebm ~/webm
   clang++ audio_decoder.cpp frame_buffer.cpp lodepng.cpp timer.cpp yuv_rgb.c file_reader.cpp gamemaker.cpp packet.cpp utils.cpp frame.cpp libpng-util.cpp player.cpp video_player.cpp -I$HOME/ -I$HOME/webm -I../include/ -o ../libWebMPlayer.so -std=c++17 -shared -I/usr/local/include -L/usr/local/lib -static -lpng -lvorbis -logg -lvpx -lz -m32 -Wno-narrowing -fPIC
 else
-  git clone https://github.com/webmproject/libwebm /c/webm
-  g++ audio_decoder.cpp frame_buffer.cpp lodepng.cpp timer.cpp yuv_rgb.c file_reader.cpp gamemaker.cpp packet.cpp utils.cpp frame.cpp libpng-util.cpp player.cpp video_player.cpp -I/c/ -I/c/webm -I../include/ -o ../libWebMPlayer.dll -std=c++17 -shared -static-libgcc -static-libstdc++ -static -lpng -lvorbis -logg -lwebm -lvpx -lz -m32 -Wno-narrowing
+  g++ audio_decoder.cpp frame_buffer.cpp lodepng.cpp timer.cpp yuv_rgb.c file_reader.cpp gamemaker.cpp packet.cpp utils.cpp frame.cpp libpng-util.cpp player.cpp video_player.cpp -I../include/ -o ../libWebMPlayer.dll -std=c++17 -shared -static-libgcc -static-libstdc++ -static -lpng -lvorbis -logg -lwebm -lvpx -lz -m32 -Wno-narrowing
 fi
