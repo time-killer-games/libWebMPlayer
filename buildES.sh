0c090e9fb28e02d777d5c404c2b77a3a883d0859
time-killer-games <samuelvenable@hotmail.com>
2021-04-16 22:37:12 -0400

    Add files via upload

--- buildES.sh
+++ buildES.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 cd "${0%/*}"
+cd "src"
 
-if [ $(uname) = "Linux" ]; then
-  g++ "src/*" -I./include/ -o libWebMPlayer.so -std=c++17 -shared -static-libgcc -static-libstdc++ -static -lpng -lvorbis -logg -lwebm -lvpx -lz -Wno-narrowing
+if [ $(uname) = "Darwin" ]; then
+  clang++ audio_decoder.cpp frame_buffer.cpp lodepng.cpp timer.cpp yuv_rgb.c file_reader.cpp gamemaker.cpp packet.cpp utils.cpp frame.cpp libpng-util.cpp player.cpp video_player.cpp -I../include/ -o ../libWebMPlayer.dylib -std=c++17 -shared -I/usr/local/include -L/usr/local/lib -static -lpng -lvorbis -logg -lwebm -lvpx -lz -Wno-narrowing -fPIC
+elif [ $(uname) = "Linux" ]; then
+  g++ audio_decoder.cpp frame_buffer.cpp lodepng.cpp timer.cpp yuv_rgb.c file_reader.cpp gamemaker.cpp packet.cpp utils.cpp frame.cpp libpng-util.cpp player.cpp video_player.cpp -I../include/ -o ../libWebMPlayer.so -std=c++17 -shared -static-libgcc -static-libstdc++ -static -lpng -lvorbis -logg -lwebm -lvpx -lz -Wno-narrowing
 elif [ $(uname) = "FreeBSD" ]; then
-  clang++ "src/*" -I./include/ -o libWebMPlayer.so -std=c++17 -shared -I/usr/local/include -L/usr/local/lib -static -lpng -lvorbis -logg -lwebm -lvpx -lz -Wno-narrowing -fPIC
+  clang++ audio_decoder.cpp frame_buffer.cpp lodepng.cpp timer.cpp yuv_rgb.c file_reader.cpp gamemaker.cpp packet.cpp utils.cpp frame.cpp libpng-util.cpp player.cpp video_player.cpp -I../include/ -o ../libWebMPlayer.so -std=c++17 -shared -I/usr/local/include -L/usr/local/lib -static -lpng -lvorbis -logg -lwebm -lvpx -lz -Wno-narrowing -fPIC
+else
+  g++ audio_decoder.cpp frame_buffer.cpp lodepng.cpp timer.cpp yuv_rgb.c file_reader.cpp gamemaker.cpp packet.cpp utils.cpp frame.cpp libpng-util.cpp player.cpp video_player.cpp -I../include/ -o ../libWebMPlayer.dll -std=c++17 -shared -static-libgcc -static-libstdc++ -static -lpng -lvorbis -logg -lwebm -lvpx -lz -Wno-narrowing
 fi
