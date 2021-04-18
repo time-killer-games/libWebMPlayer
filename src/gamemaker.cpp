8168752b3b3f3d8ad7b2be0a8375e5e94f759329
time-killer-games <samuelvenable@hotmail.com>
2021-04-18 16:56:56 -0400

    Add files via upload

--- src/gamemaker.cpp
+++ src/gamemaker.cpp
@@ -21,17 +21,13 @@
 
 static int id = -1;
 static std::unordered_map<int, uvpx::Player *> videos;
-static std::unordered_map<int, float> videos_time;
-static float gamespeed = 60.0f;
-static float gametimer = 0.0f;
 
 static void convert_rgb_to_rgba(const uint8_t *RGB, uint32_t width, uint32_t height, uint8_t **RGBA, uint32_t dispWidth, uint32_t dispHeight) {
   if ((*RGBA) == nullptr) {
     printf("RGBA buffer given is nullptr, allocating a new one.\n");
     *RGBA = (uint8_t *)malloc(4 * dispWidth * dispHeight);
   }
-  
-  //printf("RGBA=%p\n",*RGBA);
+  // printf("RGBA=%p\n", *RGBA);
   for(uint32_t y = 0; y < height; ++y) {
     if (y > dispHeight) break;
     for(uint32_t x = 0; x < width; ++x) {
@@ -44,25 +40,6 @@ static void convert_rgb_to_rgba(const uint8_t *RGB, uint32_t width, uint32_t hei
   }
 }
 
-EXPORTED_FUNCTION double video_set_gamespeed(double spd) {
-  double oldgamespeed = gamespeed;
-  gamespeed = (float)spd;
-  return oldgamespeed;
-}
-
-EXPORTED_FUNCTION double video_set_gametimer(double timer) {
-  double oldgametimer = gametimer;
-  gametimer = (float)timer;
-  return oldgametimer;
-}
-
-EXPORTED_FUNCTION double video_advance_time(double ind) { // TODO: properly finish timing stuff
-  //videos[(int)ind]->update(videos_time[(int)ind]);
-  videos[(int)ind]->update(0);
-  float endTime = gametimer / 1000.0f;
-  return 1.0;
-}
-
 EXPORTED_FUNCTION double video_exists(double ind) {
   return (videos.find(ind) != videos.end());
 }
@@ -70,6 +47,7 @@ EXPORTED_FUNCTION double video_exists(double ind) {
 EXPORTED_FUNCTION double video_add(char *fname) {
   uvpx::Player *player = new uvpx::Player(uvpx::Player::defaultConfig());
   uvpx::Player::LoadResult res = player->load(fname, 1, true);
+  id++; videos.insert(std::make_pair(id, player));
   switch (res) {
    case uvpx::Player::LoadResult::FileNotExists:
     printf("Failed to open video file '%s'\n", fname);
@@ -78,10 +56,18 @@ EXPORTED_FUNCTION double video_add(char *fname) {
    case uvpx::Player::LoadResult::NotInitialized:
     printf("Video player not initialized\n");
    case uvpx::Player::LoadResult::Success:
-    id++; videos.insert(std::make_pair(id, player)); videos_time[id] = 1.0f / gamespeed;
-    printf("Video loaded successfully\n");	  
+    printf("Video loaded successfully\n");    
   }
   return id;
+}
+
+EXPORTED_FUNCTION double video_delete(double ind) {
+  if (video_exists(ind)) {
+    delete videos[ind];
+    videos[ind] = nullptr;
+    return true;
+  }
+  return false;
 }
 
 EXPORTED_FUNCTION double video_is_playing(double ind) {
@@ -127,10 +113,10 @@ EXPORTED_FUNCTION double video_stop(double ind) {
 }
 
 // Also mirror this into the extension constants:
-const int WEBM_INVALID = 0; // reserved.
-const int WEBM_WIDTH   = 1;
-const int WEBM_HEIGHT  = 2;
-const int WEBM_TIME    = 3;
+const int WEBM_INVALID  = 0; // reserved.
+const int WEBM_WIDTH    = 1;
+const int WEBM_HEIGHT   = 2;
+const int WEBM_DURATION = 3;
 
 EXPORTED_FUNCTION double video_get_property(double ind, double propertyId) {
   if (video_exists(ind)) {
@@ -138,20 +124,17 @@ EXPORTED_FUNCTION double video_get_property(double ind, double propertyId) {
     yuv = videos[(int)ind]->lockRead();
     if (yuv) {
       double ret;
-	    switch ((int)propertyId) {
-	      case WEBM_WIDTH:  ret = yuv->displayWidth();  break;
-	      case WEBM_HEIGHT: ret = yuv->displayHeight(); break;
-	      case WEBM_TIME:   ret = yuv->time();          break;
-	      default:          ret = -1.0;                 break;
-	    }
-	    videos[(int)ind]->unlockRead();
-	    
-	    return ret;
+      switch ((int)propertyId) {
+        case WEBM_WIDTH:    ret = yuv->displayWidth();          break;
+        case WEBM_HEIGHT:   ret = yuv->displayHeight();         break;
+        case WEBM_DURATION: ret = videos[ind]->info().duration; break;
+        default:            ret = -1.0;                         break;
+      }
+      videos[(int)ind]->unlockRead();
+      return ret;
     }
-    
     return -2.0;
   }
-  
   return -3.0;
   // -1 - unknown property
   // -2 - unable to lock yuv video
@@ -166,13 +149,13 @@ EXPORTED_FUNCTION double video_get_height(double ind) {
   return video_get_property(ind, WEBM_HEIGHT);
 }
 
-EXPORTED_FUNCTION double video_get_time(double ind) {
-  return video_get_property(ind, WEBM_TIME);
+EXPORTED_FUNCTION double video_get_duration(double ind) {
+  return video_get_property(ind, WEBM_DURATION);
 }
 
-EXPORTED_FUNCTION double video_grab_frame(double ind, char *fname) {
+EXPORTED_FUNCTION double video_grab_frame_image(double ind, char *fname) {
   if (video_exists(ind)) {
-    video_advance_time(ind);
+    videos[(int)ind]->update(0);
     uvpx::Frame *yuv = nullptr;
     yuv = videos[(int)ind]->lockRead();
     if (yuv) {
@@ -184,7 +167,6 @@ EXPORTED_FUNCTION double video_grab_frame(double ind, char *fname) {
         convert_rgb_to_rgba(rgb, yuv->width(), yuv->height(), &rgba, yuv->displayWidth(), yuv->displayHeight());
         free(rgb);
         videos[(int)ind]->unlockRead();
-        
         if (rgba) {
           #if defined(_WIN32)
           std::wstring wstr = widen(fname);
@@ -193,25 +175,20 @@ EXPORTED_FUNCTION double video_grab_frame(double ind, char *fname) {
           lodepng_encode32_file(fname, rgba, yuv->displayWidth(), yuv->displayHeight());
           #endif
           free(rgba);
-          
           return 1.0;
         }
-        
         return 0.0;
       }
-      
       return -1.0;
     }
-    
     return -2.0;
   }
-  
   return -3.0;
 }
 
 EXPORTED_FUNCTION double video_grab_frame_buffer(double ind, char *buffer) {
   if (video_exists(ind)) {
-    video_advance_time(ind);
+    videos[(int)ind]->update(0);
     uvpx::Frame *yuv = nullptr;
     yuv = videos[(int)ind]->lockRead();
     if (yuv) {
@@ -223,15 +200,11 @@ EXPORTED_FUNCTION double video_grab_frame_buffer(double ind, char *buffer) {
         convert_rgb_to_rgba(rgb, yuv->width(), yuv->height(), &rgba, yuv->displayWidth(), yuv->displayHeight());
         free(rgb);
         videos[(int)ind]->unlockRead();
-        
         return 1.0;
       }
-      
       return -1.0;
     }
-    
     return -2.0;
   }
-  
   return -3.0;
 }
