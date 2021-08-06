8afe7c541a449dd3e91a3508694e7440b7e179ab
time-killer-games <samuelvenable@hotmail.com>
2021-08-06 19:34:50 -0400

    Create libwebmplayer.h

---
+++ include/libwebmplayer.h
@@ -0,0 +1,26 @@
+namespace libwebmplayer {
+
+const int WEBM_INVALID  = 0;
+const int WEBM_WIDTH    = 1;
+const int WEBM_HEIGHT   = 2;
+const int WEBM_PLAYTIME = 3;
+const int WEBM_DURATION = 4;
+
+bool video_exists(int ind);
+int video_add(const char *fname);
+bool video_delete(int ind);
+bool video_is_playing(int ind);
+bool video_play(int ind);
+bool video_is_paused(int ind);
+bool video_pause(int ind);
+bool video_is_stopped(int ind);
+bool video_stop(int ind);
+double video_get_property(int ind, int prop);
+int video_get_width(int ind);
+int video_get_height(int ind);
+double video_get_playtime(int ind);
+double video_get_duration(int ind);
+bool video_grab_frame_image(int ind, const char *fname);
+bool video_grab_frame_buffer(int ind, unsigned char *buffer);
+
+} // namespace libwebmplayer
