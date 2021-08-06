2e17b6fcf0fdb69a4f11cfa14fd2f6863dbd50e1
time-killer-games <samuelvenable@hotmail.com>
2021-08-06 19:52:49 -0400

    Update build32.sh

--- build32.sh
+++ build32.sh
@@ -11,6 +11,9 @@ elif [ $(uname) = "Linux" ]; then
 elif [ $(uname) = "FreeBSD" ]; then
   git clone https://github.com/time-killer-games/libwebm ~/webm;cmake ~/webm -DCMAKE_BUILD_TYPE=release;make
   clang++ audio_decoder.cpp frame_buffer.cpp lodepng.cpp timer.cpp yuv_rgb.cpp file_reader.cpp gamemaker.cpp packet.cpp utils.cpp frame.cpp libpng-util.cpp player.cpp video_player.cpp webm_player.cpp libwebm.a -I$HOME/ -I$HOME/webm -I../include/ -o ../libWebMPlayer.so -std=c++17 -shared -I/usr/local/include -L/usr/local/lib -lvorbis -logg -lvpx -lz -lc `pkg-config --cflags --libs sdl2 --static` -lpthread -lc -m32 -Wno-narrowing -fPIC
+elif [ $(uname) = "DragonFly" ]; then
+  git clone https://github.com/time-killer-games/libwebm ~/webm;cmake ~/webm -DCMAKE_BUILD_TYPE=release;make
+  g++ audio_decoder.cpp frame_buffer.cpp lodepng.cpp timer.cpp yuv_rgb.cpp file_reader.cpp gamemaker.cpp packet.cpp utils.cpp frame.cpp libpng-util.cpp player.cpp video_player.cpp webm_player.cpp libwebm.a -I$HOME/ -I$HOME/webm -I../include/ -o ../libWebMPlayer.so -std=c++17 -shared -I/usr/local/include -L/usr/local/lib -static-libgcc -static-libstdc++ -lvorbis -logg -lvpx -lz `pkg-config --cflags --libs sdl2 --static` -m32 -Wno-narrowing -fPIC
 else
   g++ audio_decoder.cpp frame_buffer.cpp lodepng.cpp timer.cpp file_reader.cpp gamemaker.cpp packet.cpp utils.cpp frame.cpp libpng-util.cpp player.cpp video_player.cpp webm_player.cpp widen_narrow.cpp -I../include/ -o ../libWebMPlayer.dll -std=c++17 -shared -static-libgcc -static-libstdc++ -static -lpng -lvorbis -logg -lwebm -lvpx -lz `pkg-config --cflags --libs sdl2 --static` -m32 -Wno-narrowing
 fi
